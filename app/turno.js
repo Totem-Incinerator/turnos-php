01656d8a68e9ef48f35ea03f17cbcc123a5e24c9
Arley-Anacona-Anacona <arleyanacona2017@gmail.com>
2024-06-27 17:01:09 -0500

    vos de los turnos

--- app/turno.js
+++ app/turno.js
@@ -6,14 +6,26 @@ document.addEventListener('DOMContentLoaded', function() {
 	// console.log(infoturno);
 
 	infoturno.forEach((e) => {
-
+		
 		e.addEventListener("click",() =>{
+			
 			let padre =e.parentElement.parentElement
 			console.log(padre.children[2])
-			localStorage.setItem("nombre",padre.children[1].innerHTML)
+			if(localStorage.length ===  0){
+				localStorage.setItem("nombre",padre.children[1].innerHTML)
+			    localStorage.setItem("apellido",padre.children[2].innerHTML)
+			}else{
+				localStorage.clear()
+				localStorage.setItem("nombre",padre.children[1].innerHTML)
+			    localStorage.setItem("apellido",padre.children[2].innerHTML)
+			}
+			
+			
+			// let turnoId = e.getAttribute('data-id');
+			
 
 		});
-	})
+	});
 
 	
     // Create an input element
@@ -23,6 +35,9 @@ document.addEventListener('DOMContentLoaded', function() {
 	
     modalForm.addEventListener('show.bs.modal',function(event) {
     	//event.preventDefault();
+		// localStorage.clear();
+		
+		console.log(localStorage.length);
         var button = event.relatedTarget;
         var turnoId = button.getAttribute('data-id');
 		console.log(button.getAttribute(""))
@@ -34,15 +49,17 @@ document.addEventListener('DOMContentLoaded', function() {
 		input.setAttribute("value", `${turnoId}`);
 	// Append the input element to the form
 		form_modal.appendChild(input);
-        //console.log(turnoId);
+
+       
+		speakTurn(localStorage.getItem("nombre"), localStorage.getItem("apellido"), turnoId)
+
     });
 
-	function speakTurn(turn) {
-		const msg = new SpeechSynthesisUtterance();
-		msg.text = `Atendiendo turno número: ${turn.turnType} ${turn.id}. Nombre: ${turn.name} ${turn.surname}`;
-		window.speechSynthesis.speak(msg);
-	}
-
+	function speakTurn(nombre,apellido,turno) {
+        const msg = new SpeechSynthesisUtterance();
+        msg.text = `Atendiendo turno número: ${turno}. Nombre: ${nombre}${apellido}`;
+        window.speechSynthesis.speak(msg);
+    }
 	
 	
 });
